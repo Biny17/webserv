fc2ba61141dacbe08383f8ca0775ec164a472dc8
Tom Pinton <tom.snak00s@gmail.com>
2025-10-23 13:48:31 +0200

    fonction de check de la method de la request fini

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -2,8 +2,6 @@
 
 std::vector<Server>	servers;
 
-int	check_allowed_methods(Server const &server, std::string const &req_path, std::string const &req_method);
-
 void	print_servers(std::vector<Server> servers)
 {
 	std::vector<Server>::iterator	it;
@@ -21,7 +19,7 @@ int	main(int ac, char **av)
 		return (1);
 	}
 
-	// try {
+	try {
 		try {
 			parse_conf(av[1], servers);
 		}
@@ -30,15 +28,11 @@ int	main(int ac, char **av)
 			return (1);
 		}
 		// print_servers(servers);
-		// launch_server(servers);
-	// }
-	// catch (std::exception& e) {
-	// 	std::cout << "An error has occured: " << e.what() << std::endl;
-	// 	return (1);
-	// }
-
-	//test de GET /cgi-bin/python.py HTTP/1.1
-	int check = check_allowed_methods(servers[0], "/cgi-bin/python.py", "GET");
-	std::cout << check << std::endl;
+		launch_server(servers);
+	}
+	catch (std::exception& e) {
+		std::cout << "An error has occured: " << e.what() << std::endl;
+		return (1);
+	}
 	return (0);
 }
